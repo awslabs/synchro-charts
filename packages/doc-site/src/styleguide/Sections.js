83fd9c76ef87f2ad645d65be4d8e5b3cfb58892f
Brian Diehr <diehbria@amazon.com>
2021-07-07 08:24:42 -0700

    remove unused doc-site docs, and added more descriptive warning for incorrect usage of webgl-context

--- packages/doc-site/src/styleguide/Sections.js
+++ packages/doc-site/src/styleguide/Sections.js
@@ -1,12 +1,13 @@
 import '@synchro-charts/core/dist/synchro-charts/synchro-charts.css';
 import React from 'react';
 import DefaultSectionsRenderer from 'react-styleguidist/lib/client/rsg-components/Sections/SectionsRenderer';
+import { WebglContext } from '@synchro-charts/react';
 import './Sections.css';
 
 const Sections = ({ children }) => (
     <div className="section">
         <DefaultSectionsRenderer>{children}</DefaultSectionsRenderer>
-        <sc-webgl-context />
+        <WebglContext />
     </div>
 );
 
