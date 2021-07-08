8d7d9cf6db2932ab891f17fe4165537919c932fc
Jane Zhang <zhangjane10@gmail.com>
2021-07-08 15:08:05 -0700

    add tests

--- packages/synchro-charts/src/components/charts/sc-heatmap/heatmapUtil.ts
+++ packages/synchro-charts/src/components/charts/sc-heatmap/heatmapUtil.ts
@@ -1,4 +1,4 @@
-import { DataStream, Primitive, ViewPort, DataPoint} from '../../../utils/dataTypes';
+import { DataStream, DataPoint } from '../../../utils/dataTypes';
 
 export type HeatValueMap = {
   [xBucketRangeStart: number]: {
@@ -8,15 +8,14 @@ export type HeatValueMap = {
     };
   };
 };
-const bucketCount = 10;
 
-const calculateBucketIndex = (yValue: number, yMax: number, bucketCount: number): number => {
-  return Math.ceil((yValue / yMax) * bucketCount);
+const calculateBucketIndex = (yValue: number, yMax: number, yMin: number, bucketCount: number): number => {
+  return Math.ceil(((yValue - yMin) / (yMax - yMin)) * bucketCount);
 };
 
 /**
  * Keeps track of the total number of data points within a point's respective bucket and
- * datastream name. 
+ * datastream name.
  * @param oldHeatValue HeatValueMap object with the previous heatValues.
  * @param xBucketRangeStart Lower end of the x-axis bucket range.
  * @param bucketIndex Index of the bucket that the point resides in based on its y-value.
@@ -24,15 +23,21 @@ const calculateBucketIndex = (yValue: number, yMax: number, bucketCount: number)
  * @returns Updated HeatValueMap object with the new data added -- not destructive.
  * @source deep copy: https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/
  */
-export const addCount = (oldHeatValue: HeatValueMap = {}, xBucketRangeStart: number, bucketIndex: number, dataStreamName:string): HeatValueMap => {
+export const addCount = (
+  oldHeatValue: HeatValueMap = {},
+  xBucketRangeStart: number,
+  bucketIndex: number,
+  dataStreamName: string
+): HeatValueMap => {
   // deep copy of oldHeatValue to newHeatValue
   if (!dataStreamName) {
-	  return {};
+    return {};
   }
   const newHeatValue: HeatValueMap = JSON.parse(JSON.stringify(oldHeatValue));
   newHeatValue[xBucketRangeStart] = newHeatValue[xBucketRangeStart] ?? {};
   newHeatValue[xBucketRangeStart][bucketIndex] = newHeatValue[xBucketRangeStart][bucketIndex] ?? { totalCount: 0 };
-  newHeatValue[xBucketRangeStart][bucketIndex][dataStreamName] = newHeatValue[xBucketRangeStart][bucketIndex][dataStreamName] ?? 0;
+  newHeatValue[xBucketRangeStart][bucketIndex][dataStreamName] =
+    newHeatValue[xBucketRangeStart][bucketIndex][dataStreamName] ?? 0;
   newHeatValue[xBucketRangeStart][bucketIndex][dataStreamName] += 1;
   newHeatValue[xBucketRangeStart][bucketIndex].totalCount += 1;
   return newHeatValue;
@@ -44,24 +49,33 @@ export const addCount = (oldHeatValue: HeatValueMap = {}, xBucketRangeStart: num
  * @param dataPoint DataPoint object with the x and y values of the point.
  * @param xBucketRangeStart Index of the bucket that the point resides in based on its y-value.
  * @param yMax Maximum y-value on the y-axis.
+ * @param yMin Minimum y-value on the y-axis.
  * @param dataStreamName Name of the datastream that the point belongs to.
  * @returns Updated HeatValueMap object with the new data added -- not destructive.
  */
-const countFunc = (oldHeatValue: HeatValueMap, dataPoint: DataPoint, xBucketRangeStart: number, yMax: number, dataStreamName: string): HeatValueMap => {
+const countFunc = (
+  oldHeatValue: HeatValueMap,
+  dataPoint: DataPoint,
+  xBucketRangeStart: number,
+  yMax: number,
+  yMin: number,
+  dataStreamName: string,
+): HeatValueMap => {
   const { y } = dataPoint;
-  const bucketIndex = calculateBucketIndex(parseInt(y.toString()), yMax, bucketCount);
+  const bucketIndex = calculateBucketIndex(parseInt(y.toString(), 10), yMax, yMin, 10);
   return addCount(oldHeatValue, xBucketRangeStart, bucketIndex, dataStreamName);
 };
 
 /**
  * Iterates through the points of all the datastreams and find the x-axis bucket of each data point.
- * @param heatValue Given HeatValueMap object, default is an empty map but otherwise used so that some 
+ * @param heatValue Given HeatValueMap object, default is an empty map but otherwise used so that some
  * buckets possibly don't have to be recalculated.
  * @param dataStreams DataStream array object that's passed into Mesh.
  * @param resolution Resolution of the graph's view.
  * @param startTime Left most value on the x-axis.
  * @param endTime Right most value on the x-axis.
  * @param yMax Maximum y-value on the y-axis.
+ * @param yMin Minimum y-value on the y-axis.
  * @returns Updated HeatValueMap with the aggregated data from the dataStreams.
  */
 export const calcHeatValues = (
@@ -71,29 +85,28 @@ export const calcHeatValues = (
   startTime: number,
   endTime: number,
   yMax: number,
+  yMin: number,
 ) => {
   let newHeatValue: HeatValueMap = {};
-  const XAxisBucketRange = resolution === 0 ? 1000: resolution;
-  dataStreams.forEach((dataStream) => {
-    let nextTimeStamp = startTime + XAxisBucketRange; 
-    dataStream.data.forEach((point) => {
+  const XAxisBucketRange = resolution === 0 ? 1000 : resolution;
+  dataStreams.forEach(dataStream => {
+    let nextTimeStamp = startTime + XAxisBucketRange;
+    dataStream.data.forEach(point => {
       const { x } = point;
       while (x > nextTimeStamp && nextTimeStamp < endTime) {
         nextTimeStamp += XAxisBucketRange;
       }
       // acts the same as continue in a forEach loop
       if (nextTimeStamp > endTime) {
-	return;
+        return;
       }
-      let currXBucketRangeStart = nextTimeStamp - XAxisBucketRange;
-      if (!heatValue[currXBucketRangeStart]) {
-        if (newHeatValue) {
-	  newHeatValue = countFunc(newHeatValue, point, currXBucketRangeStart, yMax, dataStream.name);
-        } else {
-	  newHeatValue = countFunc(heatValue, point, currXBucketRangeStart, yMax, dataStream.name);
-        }
+      const currXBucketRangeStart = nextTimeStamp - XAxisBucketRange;
+      if (newHeatValue) {
+        newHeatValue = countFunc(newHeatValue, point, currXBucketRangeStart, yMax, yMin, dataStream.name);
+      } else {
+        newHeatValue = countFunc(heatValue, point, currXBucketRangeStart, yMax, yMin, dataStream.name);
       }
     });
   });
-return newHeatValue;
+  return newHeatValue;
 };
