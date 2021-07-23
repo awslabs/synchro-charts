0226e6a14f9ca70f16450291e374ecd518ea1b62
Jane Zhang <zhangjane10@gmail.com>
2021-07-23 11:56:24 -0700

    revert annotation rescaling test

--- packages/synchro-charts/cypress/integration/charts/annotation-rescaling.spec.ts
+++ packages/synchro-charts/cypress/integration/charts/annotation-rescaling.spec.ts
@@ -1,41 +1,17 @@
 import { visitDynamicWidget } from '../../../src/testing/selectors';
 import { SearchQueryParams } from '../../../src/testing/dynamicWidgetUtils/testCaseParameters';
 import { X_MAX, X_MIN, Y_MAX, Y_MIN } from '../../../src/testing/test-routes/charts/constants';
-import { DataStream } from '../../../src/utils/dataTypes';
-import { DataType } from '../../../src/constants';
 
 const baseChartHeight = 500;
 const baseChartWidth = 700;
 
-const NUMBER_STREAM: DataStream = {
-  id: 'id',
-  color: 'cyan',
-  name: 'number-some-name',
-  dataType: DataType.NUMBER,
-  resolution: 0,
-  data: [
-    {
-      x: X_MAX.getTime(),
-      y: 100,
-    },
-    {
-      x: new Date(2000, 0, 1, 0, 0).getTime(),
-      y: 40,
-    },
-    {
-      x: new Date(2000, 0, 1, 2, 0).getTime(),
-      y: 60,
-    },
-  ],
-};
-
 const viewportStart = X_MIN;
 const viewportEnd = X_MAX;
 const timelineParams: Partial<SearchQueryParams> = {
-  componentTag: 'sc-bar-chart',
+  componentTag: 'sc-line-chart',
   viewportStart,
   viewportEnd,
-  dataStreams: [NUMBER_STREAM],
+  dataStreams: [],
   width: '95%',
   height: '95%',
   annotations: {
