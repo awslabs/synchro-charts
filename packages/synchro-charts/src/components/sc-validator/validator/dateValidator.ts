91e6f82731b15a51c46600dbe7842edc2b779485
Oliver <occhang@amazon.com>
2021-07-27 17:11:35 -0700

    refactor is valid date fn

--- packages/synchro-charts/src/components/sc-validator/validator/dateValidator.ts
+++ packages/synchro-charts/src/components/sc-validator/validator/dateValidator.ts
@@ -1,9 +1,3 @@
 import isISO8601 from 'validator/lib/isISO8601';
 
-export const isValidDate = (date: Date | string): boolean => {
-  if (date instanceof Date) {
-    return true;
-  }
-
-  return isISO8601(date);
-};
+export const isValidDate = (date: Date | string): boolean => (date instanceof Date ? true : isISO8601(date));
