b11f596e9b1b6f8ed901afbebd6b8682b2f77937
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2022-11-03 08:56:00 -0600

    feat: add home menu to index for local dev

---
+++ packages/synchro-charts/src/testing/testing-ground/testingGrounds.tsx
@@ -0,0 +1,29 @@
+import { Component, h } from '@stencil/core';
+import { routes } from '../app/routes';
+
+@Component({
+  tag: 'testing-ground',
+})
+export class testingGrounds {
+  render() {
+    return (
+      <div>
+        <h3>
+          hello, welcome to the{' '}
+          <a href="https://synchrocharts.com/" rel="noreferrer" target="_blank">
+            synchro-charts
+          </a>{' '}
+          testing grounds!
+        </h3>
+        <h4>check out our demo components:</h4>
+
+        {routes.map(route => (
+          <div>
+            {' '}
+            <a href={route.url}>{route.component}</a>{' '}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
