caf1fac7ac06408b9b9ea62cebaf4581b19a3b1c
tjuranek <thomas@juranek.com>
2023-03-14 14:06:58 -0600

    upgrade tippy version

--- packages/synchro-charts/src/components/common/toolTipSettings.ts
+++ packages/synchro-charts/src/components/common/toolTipSettings.ts
@@ -2,8 +2,7 @@ import { Props } from 'tippy.js';
 
 export const TIPPY_SETTINGS: Partial<Props> = {
   placement: 'right',
-  boundary: 'window',
   theme: 'title',
-  offset: '0, 10',
+  offset: [0, 10],
   duration: 0,
 };
