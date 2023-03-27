b3f342869761a7036491273e6151b63d558eaf92
Luciana Corteggiano <luciana.corteggiano@gmail.com>
2023-03-27 11:46:20 -0600

    feat: replace SC components with iot-app-kit/charts in react package

--- packages/iot-app-kit-visualizations-react/src/components.ts
+++ packages/iot-app-kit-visualizations-react/src/components.ts
@@ -3,13 +3,13 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import { JSX } from '@synchro-charts/core';
+import { JSX } from '@iot-app-kit-visualizations/core';
 
 import { defineCustomElements, applyPolyfills } from '@synchro-charts/core/dist/loader';
 
 applyPolyfills().then(() => defineCustomElements());
-export const LineChart = /*@__PURE__*/createReactComponent<JSX.ScLineChart, HTMLScLineChartElement>('iot-app-kit-vis-line-chart');
-export const ScatterChart = /*@__PURE__*/createReactComponent<JSX.ScScatterChart, HTMLScScatterChartElement>('iot-app-kit-vis-scatter-chart');
-export const BarChart = /*@__PURE__*/createReactComponent<JSX.ScBarChart, HTMLScBarChartElement>('iot-app-kit-vis-bar-chart');
-export const StatusTimeline = /*@__PURE__*/createReactComponent<JSX.ScWebglContext, HTMLScWebglContextElement>('iot-app-kit-vis-status-timeline');
-export const WebglContext = /*@__PURE__*/createReactComponent<JSX.ScWebglContext, HTMLScWebglContextElement>('iot-app-kit-vis-webgl-context');
+export const LineChart = /*@__PURE__*/createReactComponent<JSX.IotAppKitVisLineChart, HTMLIotAppKitVisLineChartElement>('iot-app-kit-vis-line-chart');
+export const ScatterChart = /*@__PURE__*/createReactComponent<JSX.IotAppKitVisScatterChart, HTMLIotAppKitVisScatterChartElement>('iot-app-kit-vis-scatter-chart');
+export const BarChart = /*@__PURE__*/createReactComponent<JSX.IotAppKitVisBarChart, HTMLIotAppKitVisBarChartElement>('iot-app-kit-vis-bar-chart');
+export const StatusTimeline = /*@__PURE__*/createReactComponent<JSX.IotAppKitVisWebglContext, HTMLIotAppKitVisWebglContextElement>('iot-app-kit-vis-status-timeline');
+export const WebglContext = /*@__PURE__*/createReactComponent<JSX.IotAppKitVisWebglContext, HTMLIotAppKitVisWebglContextElement>('iot-app-kit-vis-webgl-context');
